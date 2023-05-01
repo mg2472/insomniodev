cee8022021b0b6915e0eff4eb04d8d161493b9f3
Oscar Muñoz <oscar@b2chat.io>
2023-04-30 21:51:35 -0500

    Add theme index.php file
    - Add index.php to repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('ABSPATH')) exit; //Exit if accessed directly.
+
+/**
+ * Default blog template
+ * @package WordPress
+ * @subpackage InsomnioDev
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_template_part('templates/blog/blog', 'bootstrap-v1');
